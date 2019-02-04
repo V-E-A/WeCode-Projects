e87f1cdce33b8c34aa96e84f9acc1b9994650299
V-E-A <47076989+V-E-A@users.noreply.github.com>
2019-02-04 15:53:43 +0200

    Add files via upload

---
+++ AirBNB Clone Collaboration/client/src/api/index.js
@@ -0,0 +1,21 @@
+const baseURL = "https://abnb-clone-server-da91mqwyt.now.sh/houses/";
+
+async function getAllData() {
+  const response = await fetch(baseURL);
+  return response.json();
+}
+
+async function getOneData(id) {
+  const response = await fetch(baseURL + id);
+  return response.json();
+}
+
+function postBooking(id, bookingsNewArray) {
+  return fetch(baseURL + id, {
+    method: "PUT",
+    body: JSON.stringify({ bookings: bookingsNewArray }),
+    headers: { "Content-Type": "application/json" }
+  });
+}
+const api = { getAllData, getOneData, postBooking };
+export default api;
